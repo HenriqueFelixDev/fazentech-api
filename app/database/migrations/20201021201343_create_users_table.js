cb2ffc8a8a3dfa5df352a39a06bf29eaf35f0341
HenriqueFelixDev <henriquefelixdev@gmail.com>
2020-11-06 14:55:47 -0300

    Adição do campo photo à tabela users

--- app/database/migrations/20201021201343_create_users_table.js
+++ app/database/migrations/20201021201343_create_users_table.js
@@ -2,6 +2,7 @@
 exports.up = knex => knex.schema.createTable('users', table => {
     table.increments('id');
     table.string('name', 128).notNullable();
+    table.string('photo');
     table.string('email', 128).notNullable().unique();
     table.string('cpf', 11).notNullable().unique();
     table.string('rg', 16).notNullable();
