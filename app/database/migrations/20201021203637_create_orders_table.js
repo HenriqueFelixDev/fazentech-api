8d8fc5151d48a3c0ba93f0e561589a739c96d945
HenriqueFelixDev <henriquefelixdev@gmail.com>
2020-11-07 18:33:58 -0300

    Inserção dos campos de pagamento na tabela orders

--- app/database/migrations/20201021203637_create_orders_table.js
+++ app/database/migrations/20201021203637_create_orders_table.js
@@ -8,6 +8,16 @@ exports.up = knex => knex.schema.createTable('orders', table => {
         table.timestamp('created_in')
             .notNullable()
             .defaultTo(knex.fn.now());
+        table.enum('payment_method', [
+            'credit_card', 'bank_slip'
+        ]).defaultTo('bank_slip')
+        .notNullable();
+        table.integer('installment_count')
+            .notNullable()
+            .defaultTo(1);
+        table.string('shipping')
+            .notNullable()
+            .defaultTo('PAC');
         table.enum('status', [
             'buying', 'complete_purchase', 'preparing_for_shipment', 
             'sent', 'arrived_at_destination'
